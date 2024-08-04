570e5e030933e2611cc25ed43711e9c81406a69c
Aníbal Fuentes <Anibaaaal@gmail.com>
2024-08-04 16:28:51 -0400

    Added step calculator node
    Step calculator will provide step and start_at_step values based on a provided step count and ratio values. Similar to "denoise" slider in some samplers.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 from .resolution_picker import EasyResolutionPicker
+from .step_calculator import StepCalculator
 
 
 NODE_CLASS_MAPPINGS = {
-    "EasyResolutionPicker": EasyResolutionPicker
+    "EasyResolutionPicker": EasyResolutionPicker,
+    "StepCalculator": StepCalculator
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "EasyResolutionPicker": "Easy Resolution Picker"
+    "EasyResolutionPicker": "Easy Resolution Picker",
+    "StepCalculator": "Step Calculator"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
