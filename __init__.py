504503bffc3f5adcefa7a34e8d812d91d4223475
Aníbal Fuentes <Anibaaaal@gmail.com>
2024-12-10 16:09:09 -0300

    remove model save, implemented natively

--- __init__.py
+++ __init__.py
@@ -1,11 +1,9 @@
 from .nodes import EasyResolutionPicker, BlockLayerStringGenerator
-from .unet_save_mod import ModelSave
 from .bnb_otf import CheckpointLoaderBNB, UNETLoaderBNB
 from .merging import ModelMergeSD3_Large
 
 NODE_CLASS_MAPPINGS = {
     "EasyResolutionPicker": EasyResolutionPicker,
-    "ModelSave": ModelSave,
     "CheckpointLoaderBNB": CheckpointLoaderBNB,
     "UNETLoaderBNB": UNETLoaderBNB,
     "BlockLayerStringGenerator": BlockLayerStringGenerator,
@@ -14,7 +12,6 @@ NODE_CLASS_MAPPINGS = {
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "EasyResolutionPicker": "Easy Resolution Picker",
-    "ModelSave": "Save Diffusion Model",
     "CheckpointLoaderBNB": "Checkpoint Loader BNB On the fly",
     "UNETLoaderBNB": "UNET Loader BNB On the fly",
     "BlockLayerStringGenerator": "Flux Block Weight String",
